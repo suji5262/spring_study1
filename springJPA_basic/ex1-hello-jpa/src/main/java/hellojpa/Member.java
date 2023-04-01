d0ef270fe95a6ffe718d99fdbe372aebe7c7790d
suji5262 <suji5262@gmail.com>
2023-04-01 23:40:10 +0900

    Refactor: Member file 수정

--- springJPA_basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ springJPA_basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,21 +1,42 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+
+import javax.persistence.*;
+
+//@Entity
+//@TableGenerator(
+//        name = "MEMBER_SEQ_GENERATOR",
+//        table = "MY_SEQUENCES",
+//        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
+
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 1)
 public class Member {
 
     @Id
-    private Long id;
-    private String name;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
+    private Long id; // SEQUENCE 전략
 
-    public Member() { // jpa는 동적으로 객체를 생성해내야 함
-    }// -> 기본생성자가 있어야 한다
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id; // IDENTITY 전략
+
+
+//@Id
+//@GeneratedValue(strategy = GenerationType.TABLE,
+//        generator = "MEMBER_SEQ_GENERATOR")
+//private Long id; // TABLE 전략
+
+
+    //    @Column(unique = true, length = 10)
+    @Column(name = "name", nullable = false)
+    private String username;
 
     public Long getId() {
         return id;
@@ -25,11 +46,33 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
+
+//    private Integer age;
+//
+//    @Enumerated(EnumType.STRING)
+//    private RoleType roleType;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//
+//    private LocalDate testLocalDate;
+//    private LocalDateTime testLocalDateTime;
+//
+//    @Lob
+//    private String description;
+
+    public Member() { // jpa는 동적으로 객체를 생성해내야 함
+    }// -> 기본생성자가 있어야 한다
+
+
 }
