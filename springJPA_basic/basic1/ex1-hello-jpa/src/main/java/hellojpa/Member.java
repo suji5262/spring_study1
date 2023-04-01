333a1ffdf3cc058e6a592302d02504da6fbcc600
suji5262 <suji5262@gmail.com>
2023-04-02 03:12:18 +0900

    Feat: Member file add - 주석처리

---
+++ springJPA_basic/basic1/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,81 @@
+//package hellojpa;
+//
+//
+//import javax.persistence.*;
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.util.Date;
+//
+////@Entity
+////@TableGenerator(
+////        name = "MEMBER_SEQ_GENERATOR",
+////        table = "MY_SEQUENCES",
+////        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
+//
+//
+//@Entity
+//@SequenceGenerator(
+//        name = "MEMBER_SEQ_GENERATOR",
+//        sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
+//        initialValue = 1, allocationSize = 1)
+//public class Member {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+//            generator = "MEMBER_SEQ_GENERATOR")
+//    private Long id; // SEQUENCE 전략
+//
+//
+////    @Id
+////    @GeneratedValue(strategy = GenerationType.IDENTITY)
+////    private Long id; // IDENTITY 전략
+//
+//
+////@Id
+////@GeneratedValue(strategy = GenerationType.TABLE,
+////        generator = "MEMBER_SEQ_GENERATOR")
+////private Long id; // TABLE 전략
+//
+//
+//    //    @Column(unique = true, length = 10)
+//    @Column(name = "name", nullable = false)
+//    private String username;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+////    private Integer age;
+////
+////    @Enumerated(EnumType.STRING)
+////    private RoleType roleType;
+////
+////    @Temporal(TemporalType.TIMESTAMP)
+////    private Date createdDate;
+////
+////    @Temporal(TemporalType.TIMESTAMP)
+////    private Date lastModifiedDate;
+////
+////    private LocalDate testLocalDate;
+////    private LocalDateTime testLocalDateTime;
+////
+////    @Lob
+////    private String description;
+//
+//    public Member() { // jpa는 동적으로 객체를 생성해내야 함
+//    }// -> 기본생성자가 있어야 한다
+//
+//
+//}
