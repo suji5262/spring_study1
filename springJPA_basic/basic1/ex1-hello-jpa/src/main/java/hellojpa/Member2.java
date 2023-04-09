20961c2a06202f56501159f54924fa2bdd4c3c1d
suji5262 <suji5262@gmail.com>
2023-04-09 23:45:35 +0900

    Refactor: Member2 수정 - 공부파일 추가

--- springJPA_basic/basic1/ex1-hello-jpa/src/main/java/hellojpa/Member2.java
+++ springJPA_basic/basic1/ex1-hello-jpa/src/main/java/hellojpa/Member2.java
@@ -1,46 +1,51 @@
-package hellojpa;
-
-import javax.persistence.*;
-
-@Entity
-public class Member2 {
-
-    @Id
-    @GeneratedValue
-    @Column(name = "MEMBER_ID")
-    private Long id;
-
-    @Column(name = "USERNAME")
-    private String username;
-
-//    @Column(name = "TEAM_ID")
-//    private Long teamId;
-
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-}
+package hellojpa;//package hellojpa;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//public class Member2 {
+//
+//    @Id
+//    @GeneratedValue
+//    @Column(name = "MEMBER_ID")
+//    private Long id;
+//
+//    @Column(name = "USERNAME")
+//    private String username;
+//
+////    @Column(name = "TEAM_ID")
+////    private Long teamId;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "TEAM_ID")
+//    private Team team;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public Team getTeam() {
+//        return team;
+//    }
+//
+//    public void setTeam(Team team) {
+//        this.team = team;
+//    }
+//
+////    public void changeTeam(Team team) { //  set 을 잘 안쓰고 이렇게 바꾸자!- 권장
+////    this.team = team;
+////    team.getMember2s().add(this); // 연관관계 편의 메서드
+////    }
+//}
