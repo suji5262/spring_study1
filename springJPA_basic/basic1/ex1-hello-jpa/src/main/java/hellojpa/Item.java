848588d13bd1a65eb510cb71cfef7cd93b2ee7d3
suji5262 <suji5262@gmail.com>
2023-04-10 15:29:30 +0900

    :sparkles: Item file add

---
+++ springJPA_basic/basic1/ex1-hello-jpa/src/main/java/hellojpa/Item.java
@@ -0,0 +1,42 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+//@Inheritance(strategy = InheritanceType.JOINED) // 조인 전략
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
+public class Item {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private int price;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
