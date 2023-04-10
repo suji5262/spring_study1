28b767171e9831a116f13866d89df582b1473e7e
suji5262 <suji5262@gmail.com>
2023-04-10 15:25:57 +0900

    :bulb: Team file 수정 - 파일 주석 처리

--- springJPA_basic/basic1/ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ springJPA_basic/basic1/ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -1,47 +1,47 @@
-package hellojpa;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
-public class Team {
-
-    @Id
-    @GeneratedValue
-    @Column(name = "TEAM_ID")
-    private Long id;
-    private String name;
-
-    @OneToMany(mappedBy = "team")
-    private List<Member2> member2s = new ArrayList<>();
-
-    public void addMember(Member2 member2){
-        member2.setTeam(this);
-        member2s.add(member2);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Member2> getMember2s() {
-        return member2s;
-    }
-
-    public void setMember2s(List<Member2> member2s) {
-        this.member2s = member2s;
-    }
-}
+package hellojpa;//package hellojpa;
+//
+//import javax.persistence.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Entity
+//public class Team {
+//
+//    @Id
+//    @GeneratedValue
+//    @Column(name = "TEAM_ID")
+//    private Long id;
+//    private String name;
+//
+//    @OneToMany(mappedBy = "team")
+//    private List<Member2> member2s = new ArrayList<>();
+//
+//    public void addMember(Member2 member2){
+//        member2.setTeam(this);
+//        member2s.add(member2);
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public List<Member2> getMember2s() {
+//        return member2s;
+//    }
+//
+//    public void setMember2s(List<Member2> member2s) {
+//        this.member2s = member2s;
+//    }
+//}
