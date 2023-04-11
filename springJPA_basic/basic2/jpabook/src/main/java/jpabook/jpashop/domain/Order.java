5f69cabc2781f2f5105ba52967bef94ef7ee7d32
suji5262 <suji5262@gmail.com>
2023-04-11 20:15:23 +0900

    :sparkles: Order file add

---
+++ springJPA_basic/basic2/jpabook/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,72 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue // 생략시 Auto
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID") // MEMBER_ID 와 매핑이 됨
+    private Member member;
+
+    @OneToOne
+    @JoinColumn(name = "DELIVERT_ID")
+    private Delivery delivery;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public void addOrderItem(OrderItem orderItem){ // 연관관계 편의메서드
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
